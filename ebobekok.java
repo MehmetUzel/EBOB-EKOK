304ca9771895385404dd1e31a34949aa2df714b0
Mehmet Uzel <98mehmetuzel@gmail.com>
2019-08-30 15:37:06 +0300

    Add files via upload

---
+++ ebobekok.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+//Girilen iki sayının EBOB ve EKOK değerlerini Öklid algoritması kullanarak hesaplayan uygulama.
+
+public class ebobekok {
+    public static void main(String args[])
+    {
+        int n1,n2;
+        int ebob,ekok,kalan,bolunen,bolen;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("İki sayı giriniz :");
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        if (n1>n2)
+        {
+            bolunen=n1;
+            bolen=n2;
+        }
+        else
+        {
+            bolunen=n2;
+            bolen=n1;
+        }
+        kalan=bolunen%bolen;
+        while(kalan!=0)
+        {
+            bolunen=bolen;
+            bolen=kalan;
+            kalan=bolunen%bolen;
+        }
+        ebob = bolen;
+        ekok = n1*n2/ebob;
+        System.out.println(n1+" ve "+n2+" bu sayıların EBOB'u = "+ebob);
+        System.out.println(n1+" ve "+n2+" bu sayıların EKOK'u = "+ekok);
+    }
+}
